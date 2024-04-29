f3c7e3c247b3eb5d41299ebb9563b4bb7eae31df
Narendra Kulkarni <narkul@amazon.com>
2024-04-28 17:10:38 -0700

    Sliding Window changes

---
+++ src/com/mission/test/slidingWindow/Fixed.java
@@ -0,0 +1,41 @@
+package com.mission.test.slidingWindow;
+
+// Type 1: Problems where we are generally given a specific size of window ‘K’ in the question itself.
+public class Fixed {
+
+    // 1. Compute the result for 1st window, i.e. include the
+    //          first K elements of the data structure.
+    // 2. Then use a loop to slide the window by 1 and keep
+    //          computing the result window by window.
+
+    public int maxSum(int[] arr, int k) {
+        if (arr.length <= k) {
+            System.out.println("Invalid");
+            return -1;
+        }
+
+        // Compute sum of first window of size k
+        int max_sum = 0;
+        for (int i = 0; i < k; i++)
+            max_sum += arr[i];
+
+        // Compute sums of remaining windows by removing first element
+        // of previous window and adding last element of current window.
+        int window_sum = max_sum;
+        for (int i = k; i < arr.length; i++) {
+            // window size = i - k
+            window_sum += arr[i] - arr[i - k];
+            max_sum = Math.max(max_sum, window_sum);
+        }
+
+        return max_sum;
+    }
+
+    public static void main(String[] args) {
+        // Find the maximum sum of all sub-arrays of size K
+        Fixed f = new Fixed();
+        int[] arr = { 1, 4, 2, 10, 2, 3, 1, 0, 20 };
+        int k = 4;
+        System.out.println(f.maxSum(arr, k));
+    }
+}
---
+++ src/com/mission/test/slidingWindow/Variable.java
@@ -0,0 +1,47 @@
+package com.mission.test.slidingWindow;
+
+// Type 2: Problems in which window size is not specified
+public class Variable {
+
+    // Window = left ---- right
+    // 1. In this type of sliding window problem, we increase
+    //    our right pointer one by one till our condition is true.
+    // 2. At any step if our condition does not match, we shrink
+    //    the size of the window by increasing left pointer
+    // 3. Again when our condition satisfies, we start increasing
+    //    the right pointer and follow step 1
+    // 4. Follow the steps until end of the array is reached.
+
+    public int longestUniqueSubstring(String str) {
+        int res = 0;
+
+        for (int left = 0; left < str.length(); left++) {
+            boolean[] visited = new boolean[256];
+
+            // Here window size = 1 needs to be considered.
+            // Therefore, left = right
+            for (int right = left; right < str.length(); right++) {
+                if (visited[str.charAt(right)]) {
+                    break;
+                } else {
+                    // window size = right - left + 1
+                    res = Math.max(res, right - left + 1);
+                    visited[str.charAt(right)] = true;
+                }
+            }
+
+            visited[str.charAt(left)] = false;
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        // Given a string str, find the length of the longest substring
+        // without repeating characters.
+        Variable v = new Variable();
+        String str = "geeksforgeeks";
+        int len = v.longestUniqueSubstring(str);
+        System.out.println("The length : " + len);
+    }
+}
