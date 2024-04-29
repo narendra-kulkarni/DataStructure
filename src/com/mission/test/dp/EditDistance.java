cdabd775d9f670401e8aab9009ca3e4fa40b7466
Narendra Kulkarni <narkul@amazon.com>
2024-04-28 23:23:55 -0700

    Edit distance problem statement

--- src/com/mission/test/dp/EditDistance.java
+++ src/com/mission/test/dp/EditDistance.java
@@ -26,6 +26,14 @@ public class EditDistance {
 	}
 
 	public static void main(String[] args) {
+		// Given two strings str1 and str2 of length M and N respectively and
+		// below operations that can be performed on str1. Find the minimum
+		// number of edits (operations) to convert str1 into str2.
+
+		// Operation 1 (INSERT): Insert any character before or after any index of str1
+		// Operation 2 (REMOVE): Remove a character of str1
+		// Operation 3 (Replace): Replace a character at any index of str1 with some other character.
+
 		EditDistance l = new EditDistance();
 		String str1 = "ABE";
 		String str2 = "BE";
